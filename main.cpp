6bbea5dad16bd1d4e82020d96f09a4aeb70e1f52
elmarismayilov <elmarismaylov26@gmail.com>
2024-12-08 13:39:27 +0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,217 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <chrono>
+#include <ctime>
+#include <fstream>
+#include <sstream>
+
+class Task{
+    public:
+    Task(int i, std::string n)
+    {
+        id = i;
+        if (n.size() != 0 && n.size() <= 40)
+        {
+            name = n;
+        }
+        std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+        std::time_t now_time = std::chrono::system_clock::to_time_t(now);
+        std::tm local_time = *std::localtime(&now_time);
+        std::ostringstream oss;
+        oss << std::put_time(&local_time, "%d.%m.%Y at %I:%M%p");
+        creation_date = oss.str();
+    }
+    Task(int i, std::string n, std::string d)
+    {
+        id = i;
+        if (n.size() != 0 && n.size() <= 40)
+        {
+            name = n;
+        }
+        creation_date = d;
+    }
+    std::string get_name()
+    {
+        return name;
+    }
+    int get_id()
+    {
+        return id;
+    }
+    std::string get_creation_date()
+    {
+        return creation_date;
+    }
+    private:
+    int id;
+    std::string name;
+    std::string creation_date;
+};
+
+void display_tasks(std::vector<Task>& tasks)
+{
+    for (int i = 0; i < tasks.size(); i++)
+    {
+        std::cout << "[" << tasks[i].get_id() << "] " << tasks[i].get_name() << " | " << tasks[i].get_creation_date() << "\n";
+    }
+}
+
+void add_task(std::vector<Task>& tasks, std::string name)
+{
+    int id = tasks.size();
+    Task task(id,name);
+    tasks.push_back(task);
+}
+
+void remove_task(std::vector<Task>& tasks, int id)
+{
+    tasks.erase(tasks.begin() + id);
+}
+
+void save_ids(std::vector<Task>& tasks)
+{
+    std::ofstream ids("ids.txt");
+    int id;
+    for (int i = 0; i < tasks.size(); i++)
+    {
+        id = tasks[i].get_id();
+        ids << id << "\n";
+    }
+    ids.close();
+}
+
+void save_names(std::vector<Task>& tasks)
+{
+    std::ofstream names("names.txt");
+    std::string name;
+    for (int i = 0; i < tasks.size(); i++)
+    {
+        name = tasks[i].get_name();
+        names << name << "\n";
+    }
+    names.close();
+}
+
+void save_dates(std::vector<Task>& tasks)
+{
+    std::ofstream dates("dates.txt");
+    std::string date;
+    for (int i = 0; i < tasks.size(); i++)
+    {
+        date = tasks[i].get_creation_date();
+        dates << date << "\n";
+    }
+    dates.close();
+}
+
+void save_tasks(std::vector<Task>& tasks)
+{
+    save_ids(tasks);
+    save_names(tasks);
+    save_dates(tasks);
+}
+
+std::vector<int> load_ids()
+{
+    std::ifstream ids("ids.txt");
+    std::vector<int> i_vec;
+    int id;
+    while (ids >> id)
+    {
+        i_vec.push_back(id);
+    }
+    ids.close();
+    return i_vec;
+}
+
+std::vector<std::string> load_names()
+{
+    std::ifstream names("names.txt");
+    std::vector<std::string> n_vec;
+    std::string name;
+    while (std::getline(names, name))
+    {
+        n_vec.push_back(name);
+    }
+    names.close();
+    return n_vec;
+}
+
+std::vector<std::string> load_dates()
+{
+    std::ifstream dates("dates.txt");
+    std::vector<std::string> d_vec;
+    std::string date;
+    while (std::getline(dates, date))
+    {
+        d_vec.push_back(date);
+    }
+    dates.close();
+    return d_vec;
+}
+
+void load_tasks(std::vector<Task>& tasks)
+{
+    std::vector<int> i_vec = load_ids();
+    std::vector<std::string> n_vec = load_names();
+    std::vector<std::string> d_vec = load_dates();
+    for (int i = 0; i < i_vec.size(); i++)
+    {
+        int id = i_vec[i];
+        std::string name = n_vec[i];
+        std::string date = d_vec[i];
+        Task task(id,name,date);
+        tasks.push_back(task);
+    }
+}
+
+int main()
+{
+    std::vector<Task> tasks;
+    load_tasks(tasks);
+    int action;
+    std::cout << "Welcome to Task Management Tool!\n";
+    std::cout << "What do you want to do?\n1. Display tasks [1]\n2. Add task [2]\n3. Remove task [3]\n4. Save Tasks [4]\n5. Exit [5]\n";
+    while(true)
+    {
+        std::cout << "Action: ";
+        std::cin >> action;
+        if(std::cin.fail()) {
+            std::cin.clear(); 
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
+            std::cout << "Invalid operation!\n";
+        }
+        else
+        {
+            if (action == 1)
+            {
+                display_tasks(tasks);
+            }
+            else if(action == 2)
+            {
+                std::string a_name;
+                std::cout << "Enter a name: ";
+                std::cin.ignore();
+                std::getline(std::cin, a_name);
+                add_task(tasks, a_name);
+            }
+            else if(action == 3)
+            {
+                int r_id;
+                std::cout << "Enter id: ";
+                std::cin >> r_id;
+                remove_task(tasks,r_id);
+            }
+            else if(action == 4)
+            {
+                save_tasks(tasks);
+            }
+            else if(action == 5)
+            {
+                exit(0);
+            }
+        }
+    }
+    return 0;
+}
